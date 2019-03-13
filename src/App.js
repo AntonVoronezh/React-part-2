8a9d27666f400b8733ad6e83d5eaad919c4d4b00
AntonVoronezh <pstut@mail.ru>
2019-03-13 18:50:02 +0300

    1#1

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import "./App.css";
 import Car from "./Car/Car";
 
 class App extends Component {
-  render() {
-    const divStyle = {
-      textAlign: "center",
-      border: "1px solid red"
-    };
 
-    return (
-      <div className="App">
-        <Car name={"Ford"} year={2010} />
-        <Car name="Audi" year={2000}>
-          <p>COLOR</p>
-        </Car>
-      </div>
-    );
-  }
+
+
 }
 
 export default App;
