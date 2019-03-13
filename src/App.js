0c24f2400cdaa47189cb394ae4e83b36037ce022
AntonVoronezh <pstut@mail.ru>
2019-03-13 21:14:42 +0300

    4#1

--- src/App.js
+++ src/App.js
@@ -12,31 +12,9 @@ class App extends Component {
     title: "React Component"
   };
 
-  changeTitleHandler = () => {
-    const oldTitle = this.state.title;
 
-    const newTitle = oldTitle + " (changed)";
 
-    this.setState({
-      title: newTitle
-    });
-  };
 
-  render() {
-    const cars = this.state.car;
-
-    return (
-      <div className="App">
-        <h2>{this.state.title}</h2>
-
-        <button onClick={this.changeTitleHandler}>change title</button>
-
-        <Car name={cars[0].name} year={cars[0].year} />
-        <Car name={cars[1].name} year={cars[1].year} />
-        <Car name={cars[2].name} year={cars[2].year} />
-      </div>
-    );
-  }
 }
 
 export default App;
