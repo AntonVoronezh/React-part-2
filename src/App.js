1d28e88ba325688bb2cb07607cb477ce6bf27972
AntonVoronezh <pstut@mail.ru>
2019-03-13 18:50:19 +0300

    1#2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import "./App.css";
 import Car from "./Car/Car";
 
 class App extends Component {
-
+  state = {
+    car: [
+      {name:'Ford', year:2010},
+      {name:'Audi', year:2011},
+      {name:'Vaz', year:2012}
+    ],
+    title: 'React Component'
+  };
 
 
 }
