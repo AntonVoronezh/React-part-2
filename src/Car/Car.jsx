1f6f54ea543aad1e015c26719af50977c785d225
AntonVoronezh <pstut@mail.ru>
2019-03-14 18:41:49 +0300

    3#1

--- src/Car/Car.jsx
+++ src/Car/Car.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
-import './Car.css'
+import "./Car.css";
 
-export default props => (
-  <div className="Car">
-    <h3>Car name is {props.name}</h3>
-    <p>
-      <strong>Year: {props.year}</strong>
-    </p>
-    <input type="text" onChange={props.onChangeName} value={props.name}/>
-    <button onClick={props.onDelete}>Click</button>
-  </div>
-);
+export default props => {
+  const inputClasses = ["input"];
+
+
+
+  return (
+    <div className="Car">
+      <h3>Car name is {props.name}</h3>
+      <p>
+        <strong>Year: {props.year}</strong>
+      </p>
+      <input
+        type="text"
+        onChange={props.onChangeName}
+        value={props.name}
+      />
+      <button onClick={props.onDelete}>Click</button>
+    </div>
+  );
+};
