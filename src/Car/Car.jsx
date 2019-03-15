95038ddec6cb9c875a7cb61a994bbcb9db13024d
AntonVoronezh <pstut@mail.ru>
2019-03-15 18:25:51 +0300

    5

--- src/Car/Car.jsx
+++ src/Car/Car.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import "./Car.scss";
-import Radium from "radium";
+// import Radium from "radium";
 
 class Car extends React.Component {
+
+  componentWillReceiveProps(nextProps){
+    console.log('Car componentWillReceiveProps', nextProps)
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Car shouldComponentUpdate', nextProps, nextState)
+    return nextProps.name.trim() !== this.props.name.trim();
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('Car componentWillUpdate', nextProps, nextState)
+  }
+
+  componentDidUpdate() {
+    console.log('Car componentDidUpdate')
+  }
+
   render() {
+    console.log('Car render')
+
     const inputClasses = ["input"];
 
   if (this.props.name !== "") {
@@ -45,4 +65,5 @@ class Car extends React.Component {
  
 };
 
-export default Radium(Car);
+// export default Radium(Car);
+export default Car;
