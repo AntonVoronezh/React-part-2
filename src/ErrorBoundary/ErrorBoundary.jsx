f4409a3e75261f3f02e7392060971dd3fb194e9c
AntonVoronezh <pstut@mail.ru>
2019-03-16 17:01:51 +0300

    8#2

--- src/ErrorBoundary/ErrorBoundary.jsx
+++ src/ErrorBoundary/ErrorBoundary.jsx
@@ -9,5 +9,11 @@ export default class ErrorBoundary extends React.Component {
     this.setState({ hasError: true });
   }
 
+  render() {
+    if (this.state.hasError) {
+      return <h1 style={{ color: "red" }}>Something went wrong</h1>;
+    }
 
+    return this.props.children;
+  }
 }
