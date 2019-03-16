916f2eecd1ef26e62c74fff1cb387987996212fe
AntonVoronezh <pstut@mail.ru>
2019-03-16 17:02:34 +0300

    8#4

--- src/Car/Car.jsx
+++ src/Car/Car.jsx
@@ -36,6 +36,10 @@ class Car extends React.Component {
   render() {
     console.log('Car render')
 
+    if( Math.random() > 0.7) {
+      throw new Error('Car random failed');
+    }
+
     const inputClasses = ["input"];
 
   if (this.props.name !== "") {
