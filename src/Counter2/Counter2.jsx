211de16174ef3868e195ed39f66157bd5000e0df
AntonVoronezh <pstut@mail.ru>
2019-03-17 17:30:47 +0300

    15#1

---
+++ src/Counter2/Counter2.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default props => {
+    return (
+        <div style={{
+            border:'1px solid green',
+            width:200,
+            margin:'0 auto'
+            }}>
+            <h3>Counter 2</h3>
+            <p>Clicked</p>
+        </div>
+    )
+}
