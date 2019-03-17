18c6311f3c7e6589d1315830da30d82156b199ca
AntonVoronezh <pstut@mail.ru>
2019-03-17 16:46:06 +0300

    13#1

--- src/Car/Car.jsx
+++ src/Car/Car.jsx
@@ -1,41 +1,47 @@
 import React from "react";
 import "./Car.scss";
-import withClass from '../hoc/withClass.jsx'
+import withClass from "../hoc/withClass.jsx";
+import PropTypes from "prop-types";
 
 class Car extends React.Component {
-
   render() {
-    console.log('Car render')
+    console.log("Car render");
 
     const inputClasses = ["input"];
 
-  if (this.props.name !== "") {
-    inputClasses.push("green");
-  } else {
-    inputClasses.push("red");
-  }
+    if (this.props.name !== "") {
+      inputClasses.push("green");
+    } else {
+      inputClasses.push("red");
+    }
 
-  if (this.props.name.length > 5) {
-    inputClasses.push("bold");
-  }
+    if (this.props.name.length > 5) {
+      inputClasses.push("bold");
+    }
 
-  return (
-    <React.Fragment>
-      <h3>Car name is {this.props.name}</h3>
-      <p>
-        <strong>Year: {this.props.year}</strong>
-      </p>
-      <input
-        type="text"
-        onChange={this.props.onChangeName}
-        value={this.props.name}
-        className={inputClasses.join(" ")}
-      />
-      <button onClick={this.props.onDelete}>delete</button>
-    </React.Fragment>
-  ); 
+    return (
+      <React.Fragment>
+        <h3>Car name is {this.props.name}</h3>
+        <p>
+          <strong>Year: {this.props.year}</strong>
+        </p>
+        <input
+          type="text"
+          onChange={this.props.onChangeName}
+          value={this.props.name}
+          className={inputClasses.join(" ")}
+        />
+        <button onClick={this.props.onDelete}>delete</button>
+      </React.Fragment>
+    );
   }
- 
+}
+
+Car.propTypes = {
+  name: PropTypes.string,
+  year: PropTypes.number,
+  onChangeName: PropTypes.func,
+  onDelete: PropTypes.func
 };
 
-export default withClass(Car, 'Car');
+export default withClass(Car, "Car");
