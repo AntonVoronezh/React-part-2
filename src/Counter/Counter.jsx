2e7e8283149da9439fc1b9b58b3409c2efd737ff
AntonVoronezh <pstut@mail.ru>
2019-03-17 15:35:39 +0300

    11

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -7,9 +7,14 @@ export default class ErrorBoundary extends Component {
   };
 
   addCounter = () => {
-      this.setState({
-        counter: this.state.counter + 1
-      })
+      // this.setState({
+      //   counter: this.state.counter + 1
+      // })
+      this.setState(prevState => {
+        return {
+          counter: prevState.counter + 1
+        }
+      });
   };
 
   render() {
